369c2110224582f9c677fb093cac1da1bed1ed22
Crineth <paola.fernandez@hemoeco.com>
2026-04-10 04:50:43 -0600

    441. Adding a Custom Component To A Layout

---
+++ components/main-header.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import logoImg from "@/public/assets/logo.png"
+
+export default function MainHeader() {
+    return (
+        <header>
+            <Link href="/">
+                <img src={logoImg.src} alt="A plate with food on it" />
+                NextLevel Food
+            </Link>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="meals">Browse Meals</Link>
+                    </li>
+                    <li>
+                        <Link href="meals">Foodies Community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
